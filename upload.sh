646fddeae218ae995aae8f03f170614199ed355a
Roberto Alsina <roberto.alsina@gmail.com>
2024-07-11 14:33:20 -0300

    Minor fix in upload script

--- upload.sh
+++ upload.sh
@@ -3,6 +3,6 @@ pass github-registry | docker login ghcr.io -u ralsina --password-stdin
 docker run --rm --privileged \
         multiarch/qemu-user-static \
         --reset -p yes
-docker build . --platform=aarch64 -t ghcr.io/ralsina/caddy-static:latest --push
-docker build . -t ghcr.io/ralsina/caddy-static:latest --push
+docker build . --platform=linux/arm64 -t ghcr.io/ralsina/caddy-static-arm64:latest --push
+docker build . --platform=linux/amd64 -t ghcr.io/ralsina/caddy-static:latest --push
 
